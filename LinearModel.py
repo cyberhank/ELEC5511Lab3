608dde56ba683a1e1695c3c42c71f177e01a62f7
Henry Benerman <cyberhank@comcast.net>
2021-02-16 10:12:43 -0700

    Wrote function that finds R

--- LinearModel.py
+++ LinearModel.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import statistics as st
+import math
 
 class simpleLinearmodel():
     def __init__(self):
@@ -18,11 +19,22 @@ class simpleLinearmodel():
         print(Sxy)
         a = Sxy/Sxx
         b = ymean - (xmean*a)
+        Syy = pd.DataFrame.sum((data['Salary'] - ymean)**2)
+        data['Yhat'] = (data['YearsExperience']*a)+b
+        print(data)
         return a,b
-    def find_r(self,a,b,x):
-        yhat = pd.DataFrame()
-
-        return
+    def find_r(self,a,b,file):
+        data = pd.read_csv(file)
+        ymean = st.mean(data['Salary'])
+        ysum = pd.DataFrame.sum(data['Salary'])
+        Syy = pd.DataFrame.sum((data['Salary'] - ymean)**2)
+        data['Yhat'] = (data['YearsExperience']*a)+b
+        yhatmean = st.mean(data['Yhat'])
+        print(data)
+        Syyhat = pd.DataFrame.sum((data['Salary'] - ymean)*(data['Yhat']-yhatmean))
+        Syhatyhat =   pd.DataFrame.sum((data['Yhat'] - yhatmean)**2)
+        R = Syyhat/ math.sqrt(Syy*Syhatyhat)
+        return R
     pass
 
 
