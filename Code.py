041c96f2e1265ac4a2896c4ff6edbda1fbf51b8d
Henry Benerman <cyberhank@comcast.net>
2021-02-20 21:02:27 -0700

    Plotting stuff

--- Code.py
+++ Code.py
@@ -1,4 +1,13 @@
 from LinearModel import simpleLinearmodel
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
 newModel = simpleLinearmodel()
-newModel.linear_regression('Salary_Data.csv')
+a, b = newModel.linear_regression('Salary_Data.csv')
+data = pd.read_csv('Salary_Data.csv')
+x = np.linspace(0, 15, 100)
+y = (a*x) + b 
+plt.scatter(data['YearsExperience'],data['Salary'])
+plt.plot(x,y, 'r')
+plt.show()
